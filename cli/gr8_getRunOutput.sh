3d48c807bc4996d28f5e4eea325c7ecd13ce93ea
nagoodman <ngoodman@dynamobi.com>
2014-05-19 14:12:18 -0700

    Add in log/output access cli

---
+++ cli/gr8_getRunOutput.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/.gr8_env.sh
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: " `basename $0` "run_id"
+  exit 0
+fi
+
+RUN_URL=`curl -s -X GET \
+  -H "X-Auth-Token: $GR8_BI_TOK" \
+  $BASE_URL/current/$GR8_BI_ACT/run/$1 \
+  | egrep output_url | cut -f4 -d'"' `
+
+curl -o $1.output.zip "$RUN_URL"
