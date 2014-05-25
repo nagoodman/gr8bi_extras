8fbd3151787c8d44f0145351fc7db017d83efc26
nagoodman <ngoodman@dynamobi.com>
2014-05-25 12:04:37 -0700

    Adding update of Job Bundle!  Haaazzaaa!

---
+++ cli/gr8_updateJobBundle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/.gr8_env.sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: " `basename $0` "bun_id zipfileLocation"
+  exit 0
+fi
+
+curl -L -s -X PUT \
+  --data-binary @$2 \
+  -H 'Content-Type: application/zip' \
+  -H "X-Auth-Token: $GR8_BI_TOK" \
+  $BASE_URL/current/$GR8_BI_ACT/jobBundle/$1 | tee $DIR/.lastresponse
