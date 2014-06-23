3939e1b9a615e907399c145dca61522743184e1d
nagoodman <ngoodman@dynamobi.com>
2014-06-23 14:12:01 -0700

    Implment PATCH for Account

---
+++ cli/gr8_updateAccount.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/.gr8_env.sh
+
+if [ "$#" -ne 5 ]; then
+  echo "Usage: " `basename $0` "email password act_id key value"
+  exit 0
+fi
+
+if [ "$4" = "params" ]; then
+  SEG="\"$4\" : $5"
+else
+  SEG="\"$4\" : \"$5\""
+fi
+
+curl -s -X PATCH \
+  -H "Content-Type: application/json" \
+  -d "{ $SEG }" \
+  -u "$1:$2" \
+  "$BASE_URL/current/act/$3" | tee $DIR/.lastresponse
