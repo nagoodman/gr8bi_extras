ecda65b5d436e62eb5f7f4e8291a6f5f7909cb20
nagoodman <ngoodman@dynamobi.com>
2014-07-09 13:29:59 -0700

    if CONFIG_ENV we're in dev, and use BASE_URL

--- cli/.gr8_env.sh
+++ cli/.gr8_env.sh
@@ -6,5 +6,7 @@ export GR8_BI_ACT=`cat ~/.gr8bi_token | egrep act_id | cut -f4 -d'"'`
 
 if [ -z "$CONFIG_ENV" ]; then
   export GR8_BASE_URL=https://api.gr8.bi
+else
+  export GR8_BASE_URL=$BASE_URL
 fi
 
