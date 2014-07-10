201d344659ed0b1784b0595c2eaeba3de8225480
nagoodman <ngoodman@dynamobi.com>
2014-07-10 10:17:14 -0700

    Update for changes to stats endpoints

--- cli/gr8_statsJobDashboard.sh
+++ cli/gr8_statsJobDashboard.sh
@@ -4,10 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/.gr8_env.sh
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: " `basename $0` "[count_object_types]"
+  echo "Usage: " `basename $0` "job_id"
   exit 0
 fi
 
 curl -s -X GET \
   -H "X-Auth-Token: $GR8_BI_TOK" \
-  $GR8_BASE_URL/current/$GR8_BI_ACT/stats/$1 | tee $DIR/.lastresponse
+  $GR8_BASE_URL/current/$GR8_BI_ACT/job/$1/stats | tee $DIR/.lastresponse
---
+++ cli/gr8_statsRunDashboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/.gr8_env.sh
+
+curl -s -X GET \
+  -H "X-Auth-Token: $GR8_BI_TOK" \
+  $GR8_BASE_URL/current/$GR8_BI_ACT/run/stats | tee $DIR/.lastresponse
