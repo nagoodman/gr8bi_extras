c62ce9cb4ad2da8aed4ce9c5dc0a9e1238256fc2
nagoodman <ngoodman@dynamobi.com>
2014-07-09 22:15:01 -0700

    Add in example of time_period

--- cli/gr8_statsRun.sh
+++ cli/gr8_statsRun.sh
@@ -3,11 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/.gr8_env.sh
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: " `basename $0` "[success_status|job_id|run_status]"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: " `basename $0` "[success_status|job_id|run_status] [last_hour|last_24_hours|last_7_days|last_30_days|last_365_days]"
   exit 0
 fi
 
 curl -s -X GET \
   -H "X-Auth-Token: $GR8_BI_TOK" \
-  $GR8_BASE_URL/current/$GR8_BI_ACT/run/stats/$1 | tee $DIR/.lastresponse
+  $GR8_BASE_URL/current/$GR8_BI_ACT/run/stats/$1?time_period=$2 | tee $DIR/.lastresponse
